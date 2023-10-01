8ecf728e5aa369b439783a119fb812b7ceb7a755
Jerome <jerome.spirit@live.com>
2023-10-01 21:14:15 +0200

    Ajout du bouton de connexion

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -10,26 +10,6 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 
-//class UserType extends AbstractType
-//{
-//    public function buildForm(FormBuilderInterface $builder, array $options): void
-//    {
-//        $builder
-//            ->add('email')
-//            ->add('roles')
-//            ->add('password')
-//        ;
-//    }
-//
-//    public function configureOptions(OptionsResolver $resolver): void
-//    {
-//        $resolver->setDefaults([
-//            'data_class' => User::class,
-//        ]);
-//    }
-//}
-
-
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
