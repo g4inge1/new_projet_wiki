827bec0db2495e42cb2278f0bbebace39a35fba5
Jerome <jerome.spirit@live.com>
2023-11-09 00:21:07 +0100

    test de routage de deconnexion

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -29,10 +29,7 @@ class SecurityController extends AbstractController
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): Response
     {
-        return $this->render('acceuil/index.html.twig');
-//        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        return $this->render('accueil_auth.html.twig');
     }
-
-
 
 }
